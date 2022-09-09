d8150a724e83e187c22be42658198e41c2bdfe56
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-09-09 14:32:22 -0400

    swap function

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 
 import "fmt"
 
-func add(happy string, coding string) string {
+/*func add(happy string, coding string) string {
 	return happy + coding
 }
 func main() {
 	fmt.Println(add("I love", " elote"))
+}*/
+
+// SWAP Function
+
+func swap(k, c string) (string, string) {
+	return c, k
+}
+func main() {
+	m, n := swap("I Love", "eating snacks")
+	fmt.Println(m, n)
 }
